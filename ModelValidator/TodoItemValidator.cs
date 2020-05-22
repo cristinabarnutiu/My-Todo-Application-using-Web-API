0d9b8a98575aa6bae7940c20e024d18e3f38261d
Cristina <cristina.ilea2@yahoo.com>
2020-05-22 22:35:03 +0300

    add db context to validator

--- ModelValidator/TodoItemValidator.cs
+++ ModelValidator/TodoItemValidator.cs
@@ -1,14 +1,19 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using Test3.Models;
 
 namespace Test3.ModelValidator
 {
 	public class TodoItemValidator : AbstractValidator<TodoItem>
 	{
-		public TodoItemValidator()
+        // read more: https://www.carlrippon.com/fluentvalidation-in-an-asp-net-core-web-api/
+        public TodoItemValidator(TodoItemsDbContext context)
 		{
+			//RuleFor(x => x.MarketPrice).InclusiveBetween(5, context.Flowers.Select(f => f.MarketPrice).Max());
 			RuleFor(x => x.Id).NotNull();
 			RuleFor(x => x.Title).NotEmpty().WithMessage("Title cannot be empty");
+			//RuleFor(x => x.Title).NotEqual(context.TodoItems.Select(t => t.Title).All());
 			RuleFor(x => x.Description).Length(0,10);
 		}
 	}
